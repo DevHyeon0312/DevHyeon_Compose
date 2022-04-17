87bc5a7d10b9d723dd4f2c3c0d43665510841eab
yang-yeonghyeon <devhyeon0312@gmail.com>
2022-04-17 21:32:11 +0900

    Compose Study : ListItem Rounded

--- app/src/main/java/com/devhyeon/compose/ui/components/ListItems.kt
+++ app/src/main/java/com/devhyeon/compose/ui/components/ListItems.kt
@@ -4,10 +4,10 @@ import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons.Filled
 import androidx.compose.material.icons.filled.ExpandLess
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -26,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.devhyeon.compose.R
+import com.devhyeon.compose.ui.theme.Shapes
 
 @Preview(showBackground = true)
 @Composable
@@ -54,23 +56,21 @@ fun HomeListItemView(
     defaultExpanded: Boolean = false
 ) {
     var expanded by rememberSaveable { mutableStateOf(defaultExpanded) }
-
-    Surface(
-        color = MaterialTheme.colors.primary,
-        modifier = Modifier
-            .padding(12.dp)
-            .animateContentSize(
-                animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-                )
+    Box(modifier = Modifier
+        .padding(12.dp)
+        .background(MaterialTheme.colors.primary, RoundedCornerShape(20.dp))
+        .animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
             )
+        )
     ) {
-        Row(
-            modifier = Modifier.padding(12.dp)
-        ) {
+        Row {
             Column(
-                modifier = Modifier.weight(1f)
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(12.dp)
             ) {
                 Text(text = title)
                 Text(
